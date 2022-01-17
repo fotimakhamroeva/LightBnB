eb9ba16b75e83ef9b0c2686675949ae40f0a5112
Fotima Khamroeva <bonu_9229@bk.ru>
2022-01-16 22:26:02 -0500

    schema

---
+++ 01_schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users CASCADE;
+CREATE TABLE users () VALUES ();
+
+DROP TABLE IF EXISTS properties CASCADE;
+CREATE TABLE properties () VALUES ();
+
+DROP TABLE IF EXISTS reservations CASCADE;
+CREATE TABLE reservations () VALUES ();
+
+DROP TABLE IF EXISTS property_reviews CASCADE;
+CREATE TABLE property_reviews () VALUES ();
